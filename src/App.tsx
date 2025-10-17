e016529b221927a61a8a79489cc4ee52cac0355f
Big Sam <stosin525@gmail.com>
2025-10-17 21:56:56 +0100

    initial: Set up Vite + React + TypeScript, firebase authentication + storage, necessary dependencies, and color theme

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <button className="bg-primary text-primary-foreground px-5 py-2 m-10">
+        Click me
+      </button>
+    </>
+  );
+}
+
+export default App;
